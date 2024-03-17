3d150f7f08e7e917621b666731681a93e31da792
Minji Park <02mjpark@ewhain.net>
2024-03-18 03:12:25 +0900

    one hot encoding

--- data_.py
+++ data_.py
@@ -1,23 +1,51 @@
-import numpy as np 
-from data_dir import get_ember_data
+import numpy as np
+import torch
+import torch.nn as nn
 
+def get_ember_train_data(data_dir):
+
+    XY_train = np.load(data_dir + '/XY_train.npz')
+    X_train, Y_train = XY_train['X_train'], XY_train['Y_train']
+    Y_train = torch.LongTensor(Y_train)
+    Y_train_onehot = nn.functional.one_hot(Y_train, num_classes=100)
+    return X_train, Y_train, Y_train_onehot
+
+def get_ember_test_data(data_dir):
+    XY_test = np.load(data_dir + '/XY_test.npz')
+    X_test, Y_test = XY_test['X_test'], XY_test['Y_test']
+    Y_test = torch.LongTensor(Y_test)
+    Y_test_onehot = nn.functional.one_hot(Y_test, num_classes=100)
+
+    return X_test, Y_test, Y_test_onehot
+
+# 각 레이블 당 100개씩만 데이터 뽑기
 
 data_dir = '/home/02mjpark/continual-learning-malware/ember_data/EMBER_CL/EMBER_Class'
-X_train, Y_train, X_test, Y_test = get_ember_data(data_dir)
 
-def data_1d(data):
-    num_features = data.shape[1]
-    num_rows = len(data)
-    print('The number of the features in the EMBER dataset is', num_features) # 2381
-    print('The number of the rows in the X_train set is', len(data)) # (X_train, 303331) (X_test, 33704)
-    print('The number of the rows in the X_test set is', len(X_test))
+def extract_100data(X_train, Y_train):
 
-    return data_add
+    labels = np.unique(Y_train)
 
-X_train_conv2 = data_1d(X_train)
-X_test_conv2 = data_1d(X_test)
+    selected_indices = []
+    for label in labels:
+        label_indices = np.where(Y_train == label)[0]
+        selected_indices.extend(label_indices[:100])
+    selected_indices = np.array(selected_indices)
+    X_train_s = X_train[selected_indices]
+    Y_train_s = Y_train[selected_indices]
+    Y_train_s = torch.LongTensor(Y_train_s)
+    Y_train_s_oh = nn.functional.one_hot(Y_train_s)
+    # print("X_train_s", X_train_s.shape)
+    # unique, counts = np.unique(Y_selected, return_counts=True)
+    # print(unique, counts)
+    # print("size of Y_train_100", Y_selected.shape)
+    return X_train_s, Y_train_s, Y_train_s_oh
 
-
-    
-
-
+# X_train, Y_train, X_test, Y_test, Y_train_onehot, Y_test_onehot = get_ember_data(data_dir)
+# print("X_train.shape", X_train.shape)
+# print("Y_train", Y_train)
+# print("X_test.shape", X_test.shape)
+# print("Y_test", Y_test)
+# print("Y_train_onehot", Y_train_onehot)
+# print("Y_train_onehot", Y_train_onehot.shape)
+# print("Y_test_onehot", Y_test_onehot)
