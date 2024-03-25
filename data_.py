09a81e3601a5dc3d9c2c561608d2ffd5d3f0bbdc
Minji Park <02mjpark@ewhain.net>
2024-03-25 15:22:37 +0900

    oh

--- data_.py
+++ data_.py
@@ -1,46 +1,34 @@
-<<<<<<< HEAD
+
 import numpy as np
 import torch
 import torch.nn as nn
+import random
 
 def get_ember_train_data(data_dir):
 
     XY_train = np.load(data_dir + '/XY_train.npz')
     X_train, Y_train = XY_train['X_train'], XY_train['Y_train']
-    Y_train = torch.LongTensor(Y_train)
-    Y_train_onehot = nn.functional.one_hot(Y_train, num_classes=100)
-    return X_train, Y_train, Y_train_onehot
+
+    return X_train, Y_train
 
 def get_ember_test_data(data_dir):
     XY_test = np.load(data_dir + '/XY_test.npz')
     X_test, Y_test = XY_test['X_test'], XY_test['Y_test']
     Y_test = torch.LongTensor(Y_test)
     Y_test_onehot = nn.functional.one_hot(Y_test, num_classes=100)
-
     return X_test, Y_test, Y_test_onehot
 
-# 각 레이블 당 100개씩만 데이터 뽑기
+def shuffle_data(x_, y_):
+    indices = list(range(10000))
+    random.shuffle(indices)
+    x_ = x_[indices]
+    y_ = y_[indices]
+    return x_, y_
 
-data_dir = '/home/02mjpark/continual-learning-malware/ember_data/EMBER_CL/EMBER_Class'
-=======
-import copy
-import numpy as np
-from sklearn.utils import shuffle
-from torchvision import datasets, transforms
-from torch.utils.data import ConcatDataset, Dataset
-import torch
-from sklearn.preprocessing import StandardScaler
-
-def get_ember_data(data_dir):
-
-    XY_train = np.load(data_dir + '/XY_train.npz')
-    X_train, Y_train = XY_train['X_train'], XY_train['Y_train']
-
-    XY_test = np.load(data_dir + '/XY_test.npz')
-    X_test, Y_test = XY_test['X_test'], XY_test['Y_test']
-
-    return X_train, Y_train, X_test, Y_test
->>>>>>> 7cacb0c84a28d4f7d18f1547c8256609b0e74c19
+def oh(Y):
+    Y = torch.LongTensor(Y)
+    Y_oh = nn.functional.one_hot(Y)
+    return Y_oh
 
 def extract_100data(X_train, Y_train):
 
@@ -53,19 +41,8 @@ def extract_100data(X_train, Y_train):
     selected_indices = np.array(selected_indices)
     X_train_s = X_train[selected_indices]
     Y_train_s = Y_train[selected_indices]
-    Y_train_s = torch.LongTensor(Y_train_s)
-    Y_train_s_oh = nn.functional.one_hot(Y_train_s)
-    # print("X_train_s", X_train_s.shape)
     # unique, counts = np.unique(Y_selected, return_counts=True)
     # print(unique, counts)
     # print("size of Y_train_100", Y_selected.shape)
-    return X_train_s, Y_train_s, Y_train_s_oh
+    return X_train_s, Y_train_s
 
-# X_train, Y_train, X_test, Y_test, Y_train_onehot, Y_test_onehot = get_ember_data(data_dir)
-# print("X_train.shape", X_train.shape)
-# print("Y_train", Y_train)
-# print("X_test.shape", X_test.shape)
-# print("Y_test", Y_test)
-# print("Y_train_onehot", Y_train_onehot)
-# print("Y_train_onehot", Y_train_onehot.shape)
-# print("Y_test_onehot", Y_test_onehot)
