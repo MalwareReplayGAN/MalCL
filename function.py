e5ef7f17a8e40e4fe9541a7024fa003ebd601c0c
Minji Park <02mjpark@ewhain.net>
2024-05-04 15:35:56 +0900

    updated

--- function.py
+++ function.py
@@ -1,117 +1,90 @@
 import torch
+from torch.autograd import Variable
 import numpy as np
+import pandas
+from sklearn.preprocessing import StandardScaler
+from data_ import oh
+import time
 
 
-def get_iter_dataset(x_train, y_train, task, nb_inc=None):
+def get_iter_dataset(x_train, y_train, batchsize, task, init_classes=None, nb_inc=None):
+   
    if task is not None:
     if task == 0:
-       selected_indices = np.where(y_train < init_classes)[0]
-       return x_train[selected_indices], y_train[selected_indices]  
+       selected_indices = np.where(y_train < init_classes)[0] 
     else:
        start = init_classes + (task-1) * nb_inc
        end = init_classes + task * nb_inc
        selected_indices = np.where((y_train >= start) & (y_train < end))
-       return x_train[selected_indices], y_train[selected_indices]
     
+    x_, y_ = x_train[selected_indices], y_train[selected_indices]
 
-def run_batch(G, D, C, G_optimizer, D_optimizer, C_optimizer, x_, y_):
-      x_ = x_.view([-1, feats_length])
-      print("x_ shape", x_.shape) # [batchsize, feats_length] 16, 2381
+    # Manage Class Imbalance Issue
+    class_sample_count = np.array([len(np.where(y_ == t)[0]) for t in np.unique(y_)])
+    weight = 1. / class_sample_count
+    samples_weight = np.array([weight[t] for t in y])
+    samples_weight = torch.from_numpy(samples_weight).float()
+    sampler = torch.utils.data.WeightedRandomSampler(samples_weight, len(samples_weight), replacement=True)
+    
+    x_ = torch.from_numpy(x_).type(torch.FloatTensor)
+    y_ = torch.from_numpy(y_).type(torch.FloatTensor)
 
-      # y_real and y_fake are the label for fake and true data
-      y_real_ = Variable(torch.ones(x_.size(0), 1))
-      y_fake_ = Variable(torch.zeros(x_.size(0), 1))
-      print("y_real_shape", y_real_.shape) # [batchsize, 1] 16, 1
+    # Scaling
+    scaler = StandardScaler()
+    x_ = scaler.fit_transform(x_)
+    x_ = torch.FloatTensor(x_)
+    
+    # One-hot Encoding
+    y_oh = oh(y_, num_classes=init_classes+nb_inc*task)
+    y_oh = torch.Tensor(y_oh)
 
-      if use_cuda:
-        y_real_, y_fake_ = y_real_.cuda(0), y_fake_.cuda(0)
+    data_tensored = torch.utils.data.TensorDataset(x_, y_oh)
+    trainLoader = torch.utils.data.DataLoader(data_tensored, batch_size=batchsize, num_workers=1, sampler=sampler, drop_last=True)
 
-      z_ = torch.rand((x_.size(0), z_dim))
+    return trainLoader
 
-      x_, z_ = Variable(x_), Variable(z_)
 
-      if use_cuda:
-        x_, z_, y_ = x_.cuda(0), z_.cuda(0), y_.cuda(0)
+def ground(a):
+    new = np.zeros((a, a))
+    for i in range(a):
+        new[i][i] = 1
+    return torch.Tensor(new)
 
-      # update D network
-      D_optimizer.zero_grad()
 
-      D_real = D(x_)
-      print("D_real shape", D_real.shape) # [16, 1]
-      print("y_real_[:x_.size(0)].shape: ", y_real_[:x_.size(0)].shape) # [16, 1]
-      D_real_loss = BCELoss(D_real, y_real_[:x_.size(0)])
+def Rank(sumArr, img, y1, k):
+    start = time.time()
+    img_list = img.tolist()
+    y1_list = y1.tolist()
+    zip(img_list, y1_list)
+#    print("time for to list", time_for_to_list = time.time()-start)
+    y = pandas.DataFrame({'a': sumArr, 'b':img_list, 'c':y1_list})
+#    print("time for dataframe", time_for_dataframe = time.time()-start - time_for_to_list)
+    y = y.sort_values(by=['a'], axis = 0)
+    img_ = y['b'][0:k]
+    y1_ = y['c'][0:k]
+    return img_.tolist(), y1_.tolist()
 
-      G_ = G(z_)
-      print('G_ shape', G_.shape) # 16, 2381
-      D_fake = D(G_)
-      print("D_fake shape", D_fake.shape) # 16, 1
-      print("y_fake_[:x_.size(0)] shape", y_fake_[:x_.size(0)].shape) # 16, 1
-      D_fake_loss = BCELoss(D_fake, y_fake_[:x_.size(0)])
 
-      D_loss = D_real_loss + D_fake_loss
+def GetL2Dist(y1, y2):
+    sumArr = []
+    for i in range(len(y1)):
+        arr = []
+        for (a, b) in zip(y1[i].tolist() ,y2.tolist()):
+            arr.append((a-b)**2)
+        sumArr.append(sum(arr))
+    return sumArr
 
-      D_loss.backward()
-      D_optimizer.step()
 
-      # update G network
-      G_optimizer.zero_grad()
-
-      G_ = G(z_)
-      D_fake = D(G_)
-      G_loss = BCELoss(D_fake, y_real_[:x_.size(0)])
-
-      G_loss.backward()
-      G_optimizer.step()
-
-      # update C
-
-      C_optimizer.zero_grad()
-      print("y_ shape", y_.shape) # 16
-      output = C(x_)
-      if use_cuda:
-         output = output.cuda(0)
-
-      C_loss = criterion(output, y_)
-
-      C_loss.backward()
-      C_optimizer.step()
-
-      return output
-
-def get_replay_with_label(generator, classifier, batchsize, task, nb_inc):
-    images_list = []
-    labels_list = []
-    task_label = [[] for _ in range(init_classes + (task-1) * nb_inc)]
-
-    while True:
-        if all(len(r) >= batchsize for r in task_label):
-        # Checks whether there are at least 'batchsize' samples for each label in 'task_label'
-        # The variable 'r' represents each innter list in 'task_label'
-        # 'r' is a reference to one of the inner lists in 'task_label'
-        # The loop continues until the condition is met for all inner lists, ensuring that each label has at least 'batchsize' samples.
-            break
-        z_ = Variable(torch.rand((batchsize, z_dim)))
-
-        if use_cuda:
-            z_ = z_.cuda(0)
-
-        images = generator(z_)
-        labels = classifier.predict(images)
-        print(labels)
-        
-        for i in range(len(labels)):
-            label = labels[i]
-            # print(label)
-            if (label < (init_classes + (task-1) * nb_inc)) and (len(task_label[label]) < batchsize):
-                images_list.append(images[i].unsqueeze(0))
-                labels_list.append(label.item())
-                task_label[label].append(label.item())
-
-        for i in range(len(task_label)):
-            print("task_label:", i, "-", len(task_label[i]))
-                
-
-    images = torch.cat(images_list, dim=0)
-    labels = torch.tensor(labels_list)
-
-    return images.cpu(), labels.cpu()
+def selector(images, label, k):
+    img = []
+    lbl = []
+    lbl_for_one_hot = []
+    GroundTruth = ground(len(label[0]))
+    for i in range(len(GroundTruth)):
+        sumArr = GetL2Dist(label, GroundTruth[i])
+        new_images, new_label = Rank(sumArr, images, label, k)
+        img = img + new_images
+        lbl = lbl + new_label
+    for k in lbl:
+      lbl_for_one_hot.append(k.index(max(k)))
+    return torch.tensor(img), torch.tensor(lbl_for_one_hot)
